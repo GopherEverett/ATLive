01ec2d6c063722d65e542bceee5661cb827281f3
John Jacobs <john.a.jacobs1973@gmail.com>
2019-03-21 10:48:35 -0400

    react client app up

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require('express')
 const app = express()
-const routes = require('./routes/index')
-const methodOverride = require('method-override')
+const router = require('./routes/index')
 
 
-app.use('/', routes)
+
 app.use(express.urlencoded({ extended: true }))
-app.use(methodOverride('_method'))
 app.use(express.json())
+app.use(express.static(`${__dirname}/client/build`))
+app.use('api/ATLive/', router)
+
+app.get('/*', (req, res) => {
+    res.sendFile(`${__dirname}/client/build/index.html`)
+})
 
 const PORT = process.env.PORT || 3001
 
