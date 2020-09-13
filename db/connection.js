5584ab97a6c66362f3254128cab5edc00255c515
John Jacobs <john.a.jacobs1973@gmail.com>
2020-09-13 09:53:44 -0400

    new connection string

--- db/connection.js
+++ db/connection.js
@@ -1,24 +1,23 @@
 require('dotenv').config()
 const mongoose = require("mongoose")
 
+const connectionString = process.env.MONGODB_URI || "mongodb://localhost/ATLive";
 
-if (process.env.MONGODB_URI) {
-    mongoose.connect(process.env.MONGODB_URI);
-}
-else {
-    mongoose.connect('mongodb://localhost:27017/ATLive', {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false
-    });
-}
-mongoose.connection.on('error', function (err) {
-    console.error(`MongoDB connection error: ${err}`);
-    process.exit(-1);
-}
-);
-mongoose.connection.once('open', function () {
-    console.log("Mongoose has connected to MongoDB!");
-});
+mongoose.connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+})
+    .then(() => {
+        console.log("connected to mongo at: " + connectionString);
+    })
+// mongoose.connection.on('error', function (err) {
+//     console.error(`MongoDB connection error: ${err}`);
+//     process.exit(-1);
+// }
+// );
+// mongoose.connection.once('open', function () {
+//     console.log("Mongoose has connected to MongoDB!");
+// });
 
 module.exports = mongoose;
